83ad69a48e009a007497c952ae1382689ed45be0
Juanan Antona <juan.antonadelval@gmail.com>
2024-03-06 00:07:18 +0100

    refactor(CsvFilter): Refactors tests with constructor function

--- src/csv-filter/csv-filter-class.spec.ts
+++ src/csv-filter/csv-filter-class.spec.ts
@@ -3,12 +3,13 @@ import { csvFilter } from './csv-filter-class';
 describe('CSV Filter', () => {
   const headers =
     'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+  const emptyField = '';
   it('should return line if there is just one line and all the conditions are ok', () => {
     const inputFile = `${headers}
-1,02/05/2019,1000,810,19,,ACERLaptop,B76430134,`;
+${oneInvoice()}`;
 
     const outputFile = `${headers}
-1,02/05/2019,1000,810,19,,ACERLaptop,B76430134,`;
+${oneInvoice()}`;
 
     const filter = csvFilter.create(inputFile);
     const result = filter.filteredInvoices;
@@ -18,7 +19,7 @@ describe('CSV Filter', () => {
 
   it('should return delete invoice if there is just one line and VAT an IGIC are filled in', () => {
     const inputFile = `${headers}
-1,02/05/2019,1008,810,19,15,ACERLaptop,B76430134,`;
+    ${oneInvoice('19', '15')}`;
 
     const outputFile = `${headers}`;
     const filter = new csvFilter(inputFile);
@@ -77,4 +78,15 @@ describe('CSV Filter', () => {
     const filter = new csvFilter(inputFile);
     expect(() => filter.filteredInvoices).toThrow('error');
   });
+
+  const oneInvoice = (iva: string = '19', igic: string = emptyField) => {
+    const id = '1';
+    const date = '02/05/2019';
+    const gross = '1000';
+    const net = '810';
+    const concept = 'ACERLaptop';
+    const cif = 'B76430134';
+    const nif = emptyField;
+    return [id, date, gross, net, iva, igic, concept, cif, nif].join(',');
+  };
 });
