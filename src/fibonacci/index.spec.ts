af6fedbd459e967b630e2c4d9ab4fc3e9572696f
Juanan Antona <juan.antonadelval@gmail.com>
2024-03-26 14:25:31 +0100

    feat(Fibonacci): Adds recursive approach

--- src/fibonacci/index.spec.ts
+++ src/fibonacci/index.spec.ts
@@ -14,7 +14,7 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(1);
 
-    expect(result).toStrictEqual(0);
+    expect(result).toBe(0);
   });
 
   it('Should return 1 if 2 is provided', async () => {
@@ -22,7 +22,7 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(2);
 
-    expect(result).toStrictEqual(1);
+    expect(result).toBe(1);
   });
 
   it('Should return 1 if 3 is provided', async () => {
@@ -30,7 +30,7 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(3);
 
-    expect(result).toStrictEqual(1);
+    expect(result).toBe(1);
   });
 
   it('Should return 2 if 4 is provided', async () => {
@@ -38,7 +38,7 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(4);
 
-    expect(result).toStrictEqual(2);
+    expect(result).toBe(2);
   });
 
   it('Should return 3 if 5 is provided', async () => {
@@ -46,7 +46,7 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(5);
 
-    expect(result).toStrictEqual(3);
+    expect(result).toBe(3);
   });
 
   it('Should return 5 if 6 is provided', async () => {
@@ -54,6 +54,16 @@ describe('The fibonacci series', () => {
 
     const result = fibonacci.getElement(6);
 
-    expect(result).toStrictEqual(5);
+    expect(result).toBe(5);
+  });
+
+  it('Should return 5 if 6 is provided', async () => {
+    const fibonacci = new Fibonacci();
+
+    [6, 7, 8, 9, 10].forEach((n) => {
+      expect(fibonacci.getElement(n)).toBe(
+        fibonacci.getElement(n - 2) + fibonacci.getElement(n - 1)
+      );
+    });
   });
 });
--- src/fibonacci/index.ts
+++ src/fibonacci/index.ts
@@ -2,15 +2,8 @@ export class Fibonacci {
   constructor() {}
 
   getElement(element: number) {
-    let serie = [0];
-    for (let index = 0; index < element; index++) {
-      if (index === 0) continue;
-      if (index === 1) {
-        serie.push(1);
-        continue;
-      }
-      serie.push(serie[index - 2] + serie[index - 1]);
-    }
-    return serie[element - 1];
+    if (element === 1) return 0;
+    if (element === 2) return 1;
+    return this.getElement(element - 1) + this.getElement(element - 2);
   }
 }
