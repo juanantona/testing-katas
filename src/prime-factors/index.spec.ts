7fa21dea93ca180ae11f9b63ef2ac637a01c12c7
Juanan Antona <juan.antonadelval@gmail.com>
2024-03-28 00:23:05 +0100

    refactor(PrimeFactors): Adds final refactor

--- src/prime-factors/index.spec.ts
+++ src/prime-factors/index.spec.ts
@@ -1,6 +1,10 @@
 import { getPrimeFactorsFor } from '.';
 
 describe('Prime Factors', () => {
+  it('Should return an array with the prime one for a given number one', () => {
+    expect(getPrimeFactorsFor(1)).toStrictEqual([1]);
+  });
+
   it('Should return an array with the prime for a given prime number', () => {
     expect(getPrimeFactorsFor(2)).toStrictEqual([2]);
     expect(getPrimeFactorsFor(3)).toStrictEqual([3]);
@@ -12,5 +16,9 @@ describe('Prime Factors', () => {
 
   it('Should return an array with the prime composition sorted from smallest to biggest', () => {
     expect(getPrimeFactorsFor(3 * 5 * 7 * 11)).toStrictEqual([3, 5, 7, 11]);
+  });
+
+  it('Should throw an error when number is negative', () => {
+    expect(() => getPrimeFactorsFor(-1)).toThrow();
   });
 });
