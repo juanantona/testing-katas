1bf0a254aacd4dd1f28805bae4fa863afb5357d0
Juanan Antona <juan.antonadelval@gmail.com>
2024-04-05 17:20:53 +0200

    feat(WordWrap): Adds white spaces management

--- src/word-wrap/index.spec.ts
+++ src/word-wrap/index.spec.ts
@@ -16,5 +16,8 @@ describe('Word Wrap', () => {
     expect(wordWrap('hello', 5)).toBe('hello');
     expect(wordWrap('longword', 4)).toBe('long\nword');
     expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
+    expect(wordWrap('abc def', 4)).toBe('abc\ndef');
+    expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
+    expect(wordWrap(' abcdf', 4)).toBe('\nabcd\nf');
   });
 });
--- src/word-wrap/index.ts
+++ src/word-wrap/index.ts
@@ -1,7 +1,14 @@
 export const wordWrap = (text: string, columnSize: number): string => {
   if (text.length > columnSize) {
-    const column = text.slice(0, columnSize);
-    const rest = text.slice(columnSize);
+    let column: string, rest: string;
+    const firstWhiteSpaceIndex = text.indexOf(' ');
+    if (firstWhiteSpaceIndex > -1 && firstWhiteSpaceIndex < columnSize) {
+      column = text.slice(0, firstWhiteSpaceIndex);
+      rest = text.slice(firstWhiteSpaceIndex + 1);
+    } else {
+      column = text.slice(0, columnSize);
+      rest = text.slice(columnSize);
+    }
     return [column, wordWrap(rest, columnSize)].join('\n');
   }
   return text;
